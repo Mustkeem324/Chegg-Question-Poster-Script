c3e8b153491ea8e15706a983ab687138feb5748a
MUSTKEEM AHMAD <46074466+Mustkeem324@users.noreply.github.com>
2024-07-28 10:41:09 +0530

    Update chegg_poster.php

--- chegg_poster.php
+++ chegg_poster.php
@@ -4,20 +4,6 @@ $admin = YOUR_ID;
 $API_KEY = 'YOUR_TOKEN'; 
 define('API_KEY',$API_KEY);
 echo file_get_contents("https://api.telegram.org/bot$API_KEY/setwebhook?url=".$_SERVER['SERVER_NAME']."".$_SERVER['SCRIPT_NAME']."");
-function bot($method, $datas = []) {
-    $url = "https://api.telegram.org/bot" . API_KEY . "/" . $method;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
-    $res = curl_exec($ch);
-
-    if (curl_error($ch)) {
-        var_dump(curl_error($ch));
-    } else {
-        return json_decode($res);
-    }
-}
 
 $update = json_decode(file_get_contents('php://input'));
 $message = $update->message;
